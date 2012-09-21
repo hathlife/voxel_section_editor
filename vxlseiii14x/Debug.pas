eb2909520ef594669360e3b2c8ab390b3c3e8922
banshee <banshee@51f8c993-4936-0410-a6d7-a9c99ad3dd25>
2012-09-21 02:29:31 +0000

    This version fixes crashes when rendering at multi-section voxel models due to multiples threads trying to write debug information at the same time.
    git-svn-id: svn://svn.ppmsite.com/voxel_tools@428 51f8c993-4936-0410-a6d7-a9c99ad3dd25

--- vxlseiii14x/Debug.pas
+++ vxlseiii14x/Debug.pas
@@ -3,14 +3,16 @@ unit Debug;
 interface
 
 uses
-   Dialogs, SysUtils; // VK for EInOutError & MessageDlg
+   Dialogs, SysUtils, SyncObjs; // VK for EInOutError & MessageDlg
 
 type
    TDebugFile = class
         FileName : string;
       private
+         FLock : TCriticalSection;
       public
          constructor Create(const _FileName: String);
+         destructor Destroy; override;
          procedure Add(const _Content:string);
    end;
 
@@ -18,10 +20,13 @@ type
 
 implementation
 
+uses Windows;
+
    constructor TDebugFile.Create(const _FileName: String);
    var
       MyFile : System.Text;
    begin
+      FLock := TCriticalSection.Create;
       Filename := _Filename;
       try
          AssignFile(MyFile,Filename);
@@ -33,16 +38,25 @@ implementation
       end;
    end;
 
+   destructor TDebugFile.Destroy;
+   begin
+      FLock.Free;
+      FileName := '';
+      inherited Destroy;
+   end;
+
    procedure TDebugFile.Add(const _Content:string);
    var
       MyFile : System.Text;
    begin
       try
+         FLock.Acquire;
          AssignFile(MyFile,Filename);
          FileMode := fmOpenWrite;
          Append(MyFile);
          Writeln(MyFile,_Content);
          CloseFile(MyFile);
+         FLock.Release;
       except on E : EInOutError do // VK 1.36 U
          MessageDlg('Error: ' + E.Message + Char($0A) + Filename, mtError, [mbOK], 0);
       end;
